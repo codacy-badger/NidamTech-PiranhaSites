b46abb4c4105404f6a2951dd6964bfe443f58eba
Nikolai Damm <nikolai.damm@gmail.com>
2019-01-31 23:39:43 +0100

    #5 - Manager View finished

--- Areas/Manager/Models/ThemeListModel.cs
+++ Areas/Manager/Models/ThemeListModel.cs
@@ -6,31 +6,24 @@ using Piranha;
 namespace nidam_corp.Areas.Manager.Models
 {
     public class ThemeListModel
-    {
+    {    
         public class SiteInfo
         {
             public string Id { get; set; }
             public string Title { get; set; }
             public bool IsDefault { get; set; }
         }
-
-        public IList<Piranha.Models.PageType> PageTypes { get; set; } = new List<Piranha.Models.PageType>();
-
-        public IList<Piranha.Models.SitemapItem> Sitemap { get; set; } = new List<Piranha.Models.SitemapItem>();
-
+        public class Theme
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+        } 
+        public IList<Theme> Themes { get; set; } = new List<Theme>();
         public IList<SiteInfo> Sites { get; set; } = new List<SiteInfo>();
-
-        public string SiteId { get; set; }
-
         public string SiteTitle { get; set; }
 
-        public bool HasSiteContent { get; set; }
+        public string ThemeSelectorId { get; set; }
 
-        public Guid SiteContentId { get; set; }
-
-        public string PageId { get; set; }
-
-        public int ExpandedLevels { get; set; }
         public static ThemeListModel Get(IApi api, Guid? siteId, string pageId = null)
         {
             var model = new ThemeListModel();
@@ -43,25 +36,20 @@ namespace nidam_corp.Areas.Manager.Models
             {
                 site = defaultSite;
             }
-
-            model.SiteId = site.Id == defaultSite.Id ? "" : site.Id.ToString();
             model.SiteTitle = site.Title;
-            model.HasSiteContent = !string.IsNullOrWhiteSpace(site.SiteTypeId);
-            model.SiteContentId = site.Id;
-            model.PageId = pageId;
-            model.PageTypes = api.PageTypes.GetAll().ToList();
-            model.Sitemap = api.Sites.GetSitemap(site.Id, onlyPublished: false);
+            model.ThemeSelectorId = "Theme Selector";
+//            model.Themes = api.Themes.GetAll().Select(t => new Theme
+//            {
+//                Name = t.Name,
+//                Url = t.Url
+//            }).ToList();
             model.Sites = api.Sites.GetAll().Select(s => new SiteInfo
             {
                 Id = s.Id.ToString(),
                 Title = s.Title,
                 IsDefault = s.IsDefault
             }).ToList();
-
-            using (var config = new Config(api))
-            {
-                model.ExpandedLevels = config.ManagerExpandedSitemapLevels;
-            }
+ 
             return model;
         }
     }
