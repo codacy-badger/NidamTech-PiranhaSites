09fd669d196f14cfe9972e5fc611a056e1a14df0
Nikolai Damm <nikolai.damm@gmail.com>
2019-02-14 14:26:45 +0100

    Changes to gulp tasks

--- gulp/tasks/compile/compile-libraries.js
+++ gulp/tasks/compile/compile-libraries.js
@@ -2,7 +2,8 @@ const gulp = require('gulp'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify');
 
-const libraries = ['node_modules/jquery/dist/jquery.slim.min.js', 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'];
+const libraries = ['node_modules/jquery/dist/jquery.slim.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'];
 gulp.task('compile-libraries', function (done) {
     gulp.src(libraries)
         .pipe(rename(
